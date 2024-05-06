647798dbe0f3b7f5ccf43532315ecb5c25f895ed
RebornPowerHorse <leo@cleptrum.com>
2024-05-06 21:35:03 +0800

    added special sixth sense audio and visual

--- install_swf.sh
+++ install_swf.sh
@@ -13,3 +13,6 @@ lastversion=`(cd $resmods && ls | grep '^[0-9]\.[0-9]' | sort -V | tail -n1)`
 installdir="$resmods/$lastversion/gui/flash/"
 mkdir -pv "$installdir"
 cp -v "res/gui/flash/GUIFlash.swf" "$installdir"
+cp -v "res/gui/flash/sixthSense.swf" "$installdir"
+mkdir -pv "$installdir/../../audioww"
+cp -v "res/audioww/sixthSense.mp3" "$installdir/../../audioww/"
